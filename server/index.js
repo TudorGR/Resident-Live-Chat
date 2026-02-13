ddbd131d4e5c9b1629a82f8c45a14e76ceb6f4ae
Tudor Gradinaru <tudor.rodut@yahoo.com>
2026-02-13 16:52:11 +0200

    Initialized simple angular app + nodejs server

---
+++ server/index.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Server Running"));
+
+app.listen(5000, () => console.log("Server running on http://localhost:5000"));
